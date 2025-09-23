0760529a7a422ae4163f6096f4162755dd80ab38
Acer <rushikesh.pawar100@gmail.com>
2025-09-22 22:13:42 -0400

    second commit

--- app/Labs/Lab1/page.tsx
+++ app/Labs/Lab1/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-<h1>Labs</h1>
 
 export default function Lab1() {
   return (
@@ -8,7 +7,13 @@ export default function Lab1() {
       <h3>HTML Examples</h3>
       <div id="wd-h-tag">
         <h4>Heading Tags</h4>
-        Text documents are often broken up into several sections and subsections. Each section is usually prefaced with a short title or heading that attempts to summarize the topic of the section it precedes. For instance this paragraph is preceded by the heading Heading Tags. The font of the section headings are usually larger and bolder than their subsection headings. This document uses headings to introduce topics such as HTML Documents, HTML Tags, Heading Tags, etc. HTML heading tags can be used to format plain text so that it renders in a browser as large headings. There are 6 heading tags for different sizes: h1, h2, h3, h4, h5, and h6. Tag h1 is the largest heading and h6 is the smallest heading.
+        Text documents are often broken up into several sections and subsections. Each section is usually prefaced with
+         a short title or heading that attempts to summarize the topic of the section it precedes. For instance this 
+         paragraph is preceded by the heading Heading Tags. The font of the section headings are usually larger and 
+         bolder than their subsection headings. This document uses headings to introduce topics such as HTML Documents,
+          HTML Tags, Heading Tags, etc. HTML heading tags can be used to format plain text so that it renders in a 
+          browser as large headings. There are 6 heading tags for different sizes: h1, h2, h3, h4, h5, and h6. Tag h1 
+          is the largest heading and h6 is the smallest heading.
       </div>
       <div id="wd-p-tag">
         <h4>Paragraph Tag</h4>
@@ -57,7 +62,7 @@ export default function Lab1() {
         <ul id="wd-my-books">
           <li>Dune</li>
           <li>Lord of the Rings</li>
-          <li>Ender's Game</li>
+          <li>Enders Game</li>
           <li>Red Mars</li>
           <li>The Forever War</li>
         </ul>
