1b05889b62cdabe4c889fcde639c248b8af01207
Acer <rushikesh.pawar100@gmail.com>
2025-09-22 23:37:29 -0400

    fourth commit

--- app/(Kambaz)/Courses/[cid]/Modules/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Modules/page.tsx
@@ -4,7 +4,7 @@ export default function Modules() {
       <button>Collapse All</button> <button>View Progress</button> <button>Publish All</button> <button>+ Module</button>
       <ul id="wd-modules">
         <li className="wd-module">
-          <div className="wd-title">Week 1</div>
+          <div className="wd-title">Week 1, Lecture 1 – Course Introduction, Syllabus, Agenda</div>
           <ul className="wd-lessons">
             <li className="wd-lesson">
               <span className="wd-title">LEARNING OBJECTIVES</span>
@@ -13,28 +13,43 @@ export default function Modules() {
                 <li className="wd-content-item">Learn what is Web Development</li>
               </ul>
             </li>
-          </ul>
-        </li>
-        <li className="wd-module">
-          <div className="wd-title">Week 2</div>
-             <ul className="wd-lessons">
+
             <li className="wd-lesson">
-              <span className="wd-title">LEARNING OBJECTIVES</span>
+              <span className="wd-title">READING</span>
               <ul className="wd-content">
-                <li className="wd-content-item">Lear HTML and css</li>
-                <li className="wd-content-item">Learn tags</li>
+                <li className="wd-content-item">Full Stack Developer – Chapter 1 – Introduction</li>
+                <li className="wd-content-item">Full Stack Developer – Chapter 2 – Creating Us</li>
+              </ul>
+            </li>
+
+            <li className="wd-lesson">
+              <span className="wd-title">SLIDES</span>
+              <ul className="wd-content">
+                <li className="wd-content-item">Introduction to Web Development</li>
+                <li className="wd-content-item">Creating an HTTP server with Node.js</li>
+                <li className="wd-content-item">Creating a React Application</li>
               </ul>
             </li>
           </ul>
         </li>
+
         <li className="wd-module">
-          <div className="wd-title">Week 3</div>
-             <ul className="wd-lessons">
+          <div className="wd-title">Week 1, Lecture 2 – Formatting User Interfaces with HTML</div>
+          <ul className="wd-lessons">
             <li className="wd-lesson">
               <span className="wd-title">LEARNING OBJECTIVES</span>
               <ul className="wd-content">
-                <li className="wd-content-item">Intro to JS</li>
-                <li className="wd-content-item">Learn what is Node.js</li>
+                <li className="wd-content-item">Learn how to create user interfaces with HTML</li>
+                <li className="wd-content-item">Deploy the assignment to Netlify</li>
+              </ul>
+            </li>
+
+            <li className="wd-lesson">
+              <span className="wd-title">SLIDES</span>
+              <ul className="wd-content">
+                <li className="wd-content-item">Introduction to HTML and the DOM</li>
+                <li className="wd-content-item">Formatting Web content with Headings</li>
+                <li className="wd-content-item">Formatting content with Lists and Tables</li>
               </ul>
             </li>
           </ul>
