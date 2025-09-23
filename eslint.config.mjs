2fbf87be7cc1dedb59d00031c7fa47a677d6ccc2
Acer <rushikesh.pawar100@gmail.com>
2025-09-22 22:41:30 -0400

    thirdd commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,7 +20,8 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
     rules: {
-      "react/no-unescaped-entities": "off",  
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
     },
   },
 ];
