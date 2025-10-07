e397ae58bd1446eedcd7e84350b59827f9db51fa
Acer <rushikesh.pawar100@gmail.com>
2025-10-07 00:52:18 -0400

    a2 CSS and Bootstrapss

--- app/(Kambaz)/Account/Navigation.tsx
+++ app/(Kambaz)/Account/Navigation.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 export default function AccountNavigation() {
  return (
    <div id="wd-account-navigation">
-     <Link href="Signin"> Signin </Link> <br />
-     <Link href="Signup"> Signup </Link> <br />
-     <Link href="Profile"> Profile </Link> <br />
+     <Link href="Signin" style={{ color: "red" }}> Signin </Link> <br />
+     <Link href="Signup" style={{ color: "red" }}> Signup </Link> <br />
+     <Link href="Profile" style={{ color: "red" }}> Profile </Link> <br />
    </div>
 );}
--- app/(Kambaz)/Account/Signup/page.tsx
+++ app/(Kambaz)/Account/Signup/page.tsx
@@ -1,13 +1,14 @@
 import Link from "next/link";
+import { FormControl } from "react-bootstrap";
 export default function Signup() {
   return (
     <div id="wd-signup-screen">
       <h3>Sign up</h3>
-      <input placeholder="username" className="wd-username" /><br/>
-      <input placeholder="password" type="password" className="wd-password" /><br/>
-      <input placeholder="verify password"
+      <FormControl  id = 'wd-username' placeholder="username" className="wd-username" /><br/>
+      <FormControl id = 'wd-password' placeholder="password" type="password" className="wd-password" /><br/>
+      <FormControl id = 'wd-password-verify' placeholder="verify password"
              type="password" className="wd-password-verify" /><br/>
-      <Link  href="Profile" > Sign up </Link><br />
-      <Link  href="Signin" > Sign in </Link>
+      <Link  href="Profile" className="btn btn-primary w-100 mb-2"> Sign up </Link><br />
+      <Link  href="Signin"> Sign in </Link>
     </div>
 );}
