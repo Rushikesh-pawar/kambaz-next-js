c8aca9edc0e7193a4ae4e831f008f75a022ac358
Acer <rushikesh.pawar100@gmail.com>
2025-11-11 17:13:25 -0500

    a4 Reduxxx1

--- app/Labs/Lab4/ReduxExamples/index.tsx
+++ app/Labs/Lab4/ReduxExamples/index.tsx
@@ -2,7 +2,7 @@ import AddRedux from "./AddRedux";
 import CounterRedux from "./CounterRedux";
 import HelloRedux from "./HelloRedux";
 import TodoList from "./todos/TodoList";
-console.log("TodoList import:", typeof TodoList, TodoList);
+
 export default function ReduxExamples() {
   return(
     <div>
--- app/Labs/Lab4/ReduxExamples/todos/TodoList.tsx
+++ app/Labs/Lab4/ReduxExamples/todos/TodoList.tsx
@@ -2,17 +2,19 @@ import React from "react";
 import TodoForm from "./TodoForm";
 import TodoItem from "./TodoItem";
 import { useSelector } from "react-redux";
-import { ListGroupItem, Button, ListGroup } from "react-bootstrap";
-import { RootState } from "../../store";
+import { ListGroup } from "react-bootstrap";
 export default function TodoList() {
-  const { todos } = useSelector((state: RootState) => state.todosReducer);
+  const { todos } = useSelector((state: any) => state.todosReducer);
   return (
     <div id="wd-todo-list-redux">
       <h2>Todo List</h2>
       <ListGroup>
-        <TodoForm />
+        <TodoForm key={"td-item-1"} />
         {todos.map((todo: any) => (
-          <TodoItem todo={todo} />
+          <TodoItem
+          key={todo.id} 
+          todo={todo}
+           />
         ))}
       </ListGroup>
       <hr/>
