3984b2e49557123fda297af8a1b9599db7b78315
Acer <rushikesh.pawar100@gmail.com>
2025-11-11 17:11:31 -0500

    a4 Reduxxx

--- app/Labs/Lab4/ReduxExamples/CounterRedux/index.tsx
+++ app/Labs/Lab4/ReduxExamples/CounterRedux/index.tsx
@@ -1,15 +1,16 @@
 import { useSelector, useDispatch } from "react-redux";
 import { increment, decrement } from "./counterReducer";
-import { RootState } from "../../store";
 export default function CounterRedux() {
-  const { count } = useSelector((state: RootState) => state.counterReducer);
+  const { count } = useSelector((state: any) => state.counterReducer);
   const dispatch = useDispatch();
   return (
     <div id="wd-counter-redux">
       <h2>Counter Redux</h2>
       <h3>{count}</h3>
-      <button onClick={() => dispatch(increment())} id="wd-counter-redux-increment-click"> Increment </button>
-      <button onClick={() => dispatch(decrement())} id="wd-counter-redux-decrement-click"> Decrement </button>
+      <button onClick={() => dispatch(increment())}
+              id="wd-counter-redux-increment-click"> Increment </button>
+      <button onClick={() => dispatch(decrement())}
+              id="wd-counter-redux-decrement-click"> Decrement </button>
       <hr/>
     </div>
 );}
