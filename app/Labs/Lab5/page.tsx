879428669d7ddcc1ba109660b65a291f9d39ed04
Acer <rushikesh.pawar100@gmail.com>
2025-11-26 19:21:54 -0500

    a5 latest11

--- app/Labs/Lab5/page.tsx
+++ app/Labs/Lab5/page.tsx
@@ -1,5 +1,4 @@
 import EnvironmentVariables from "./EnvironmentVariables";
-<<<<<<< HEAD
 import PathParameters from "./PathParameters";
 import QueryParameters from "./QueryParameters";
 import WorkingWithObjects from "./WorkingWithObjects";
@@ -29,20 +28,3 @@ export default function Lab5() {
     </div>
   );
 }
-=======
-const HTTP_SERVER = process.env.NEXT_PUBLIC_HTTP_SERVER;
-export default function Lab5() {
-    return (
-      <div id="wd-lab5">
-        <h2>Lab 5</h2>
-        <div className="list-group">
-          <a href={`${HTTP_SERVER}/lab5/welcome`}          
-             className="list-group-item">
-             Welcome
-          </a>
-        </div><hr/>
-        <EnvironmentVariables />
-      </div>
-  );}
-  
->>>>>>> bec22b3 (a5 nextjs)
