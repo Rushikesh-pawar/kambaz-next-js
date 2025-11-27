e17a776026b400e0becb19a832f3e1af9f59b666
Acer <rushikesh.pawar100@gmail.com>
2025-11-26 19:13:55 -0500

    a5 latest

--- app/(Kambaz)/Database/index.ts
+++ app/(Kambaz)/Database/index.ts
@@ -1,6 +1,6 @@
-import courses from "./courses";
+import courses from "./courses.json";
 import modules from "./modules.json";
-import assignment from "./assignment";
+import assignment from "./assignment.json";
 import enrollments from "./enrollments.json";
 import users from "./users.json";
 export {  courses, modules, assignment, enrollments, users  };
